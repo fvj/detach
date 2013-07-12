e854ee6d13cdd2ef0cfa07e472aa800717071451
fvj <fvj@server.fake>
2013-07-12 16:57:25 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envp) {
+  if(fork() == 0) {
+    int i;
+    char *arguments[argc - 1];
+
+    arguments[0] = argv[1];
+
+    for(i = 1; i < argc - 1; i++)
+      arguments[i] = argv[i+1];
+    arguments[i] = NULL;
+
+    for(i = 0; i < argc - 1; i++)
+      printf("%d: %s\n", i, arguments[i]);
+
+    return execvp(argv[1], arguments);
+  }
+  return 0;
+}
